7a8f2e1a77803c94c5faf80ad09a53c583035af6
Jayachitra M <113676745+JayachitraM@users.noreply.github.com>
2024-10-05 04:26:51 -0700

    Add files via upload
    js file uploaded

---
+++ Explorerhubs/Javascript/rough.js
@@ -0,0 +1,17 @@
+function searchFunction() {
+    var input, filter, ul, li, i, txtValue;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById('list');
+    li = ul.getElementsByTagName('li');
+  
+    for (i = 0; i < li.length; i++) {
+      txtValue = li[i].textContent || li[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = '';
+      } else {
+        li[i].style.display = 'none';
+      }
+    }
+  }
+  
